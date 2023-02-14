e4e2fbe180d4cf5e16cb92f8328c35dd515db260
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2023-02-14 21:29:26 +0200

    functioning app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gce_guide/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,9 +9,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'GCE Guide',
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      home: const MainPage(
+        url: 'https://papers.gceguide.com/',
+        title: 'GCE Guide',
+      ),
     );
   }
 }
---
+++ lib/models/card_model.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class CardModel extends StatelessWidget {
+  final String name;
+  final void Function()? onTap;
+  const CardModel({super.key, required this.name, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 2.5,
+            color: SchedulerBinding.instance.window.platformBrightness ==
+                    Brightness.dark
+                ? Colors.white
+                : Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+            child: Text(
+          name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+          textScaleFactor: 1.75,
+        )),
+      ),
+    );
+  }
+}
